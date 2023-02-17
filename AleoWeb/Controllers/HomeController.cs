925393e195363537c9054961c0e3cc49c350744a
SedovAlexy <109035197+Qgoni@users.noreply.github.com>
2023-02-17 16:27:54 +0300

    Добавьте файлы проекта.

---
+++ AleoWeb/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using AleoWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AleoWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
